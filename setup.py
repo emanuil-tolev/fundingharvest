e8f610ed92e7d49afdf99349f82b81af3f72c7f7
Emanuil Tolev <emanuil.tolev@gmail.com>
2013-09-12 21:05:06 +0100

    migrate requirements to setup file

--- setup.py
+++ setup.py
@@ -4,21 +4,25 @@ setup(
     name = 'fundingharvest',
     version = '0.1',
     packages = find_packages(),
-    url = 'TODO PUT URL HERE WHEN DEPLOYED',
+    url = 'https://github.com/emanuil-tolev/fundingharvest',
     author = 'Emanuil Tolev',
     author_email = 'emanuil.tolev@gmail.com',
     description = 'Funding Harvest digests data from RSS feeds and (soon) other sources and stores it (presently just POST-s to an elasticsearch instance). Started as the data gathering piece of FundFind https://github.com/emanuil-tolev/fundfind/',
-    license = 'TODO PUT LICENCE HERE WHEN DECIDED',
-    # TODO define classifiers when decided (also look for other potentially 
-    # useful classifiers
-    # classifiers = [
-        # 'Development Status :: 3 - Alpha',
-        # 'Environment :: Console',
-        # 'Intended Audience :: Developers',
-        # 'License :: OSI Approved :: MIT License',
-        # 'Operating System :: OS Independent',
-        # 'Programming Language :: Python',
-        # 'Topic :: Software Development :: Libraries :: Python Modules'
-    # ],
+    license = 'MIT',
+    install_requires = [
+    'pyes==0.16',
+    'requests==1.1.0',
+    'parsedatetime==0.8.7',
+    'feedparser==5.1.3',
+    ]
+    # TODO look for other potentially useful classifiers
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
 )
 
